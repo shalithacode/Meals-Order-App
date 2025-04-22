e1cdd4efb443030c14331519ef3b2acd9a174c3f
shalith98 <shalithalakruwan1998@gmail.com>
2025-04-23 00:46:30 +0530

    Added reusable button component

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.jpg";
-
+import Button from "./UI/Button";
 function Header() {
   return (
     <header id="main-header">
@@ -9,7 +9,7 @@ function Header() {
         <h1>Food Hub</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
--- Frontend/src/components/MealItem.jsx
+++ Frontend/src/components/MealItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./UI/Button";
 
 function MealItem({ meal }) {
   return (
@@ -13,7 +14,7 @@ function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to cart</button>
+          <Button>Add to cart</Button>
         </p>
       </article>
     </li>
