e4fe9ee928ff7dc22bd9b0087d1995ddb82cd97c
shalith98 <shalithalakruwan1998@gmail.com>
2025-04-25 22:41:18 +0530

    added /Order post API

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
 const cors = require("cors");
 require("dotenv").config();
 
 const Menu = require("./models/meal");
-
+const Order = require("./models/order");
 const app = express();
 
 app.use(cors());
@@ -22,6 +23,52 @@ app.get("/meals", async (req, res) => {
     next(err);
   }
 });
+
+app.post(
+  "/orders",
+  [
+    body("order").notEmpty().withMessage("Order data is required."),
+    body("order.items").isArray({ min: 1 }).withMessage("Order must contain at least one item."),
+    body("order.customer.name").trim().notEmpty(),
+    body("order.customer.street").trim().notEmpty(),
+    body("order.customer.postal-code").trim().notEmpty().isInt(),
+    body("order.customer.city").trim().notEmpty(),
+    body("order.customer.email").isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed.");
+      error.statusCode = 422;
+      error.info = errors.array();
+      throw error;
+    }
+    try {
+      const orderData = req.body.order;
+
+      const customer = {
+        name: orderData.customer.name.trim(),
+        email: orderData.customer.email.trim(),
+        street: orderData.customer.street.trim(),
+        postalCode: orderData.customer["postal-code"],
+        city: orderData.customer.city.trim(),
+      };
+
+      const newOrder = new Order({
+        items: orderData.items,
+        customer: customer,
+      });
+
+      const savedOrder = await newOrder.save();
+      res.status(201).json({ message: "Order created!", order: savedOrder });
+    } catch (err) {
+      if (!err.statusCode) statusCode = 500;
+      next(err);
+    }
+  }
+);
+
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode;
@@ -29,6 +76,7 @@ app.use((error, req, res, next) => {
   const data = error.data;
   return res.status(status).json({ message, data });
 });
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.erpgzd4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
--- Backend/models/order.js
+++ Backend/models/order.js
@@ -1,24 +1,62 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const orderSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   price: {
-//     type: Number,
-//     required: true,
-//   },
-//   description: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: String,
-//     required: true,
-//   },
-// });
+const orderSchema = new Schema(
+  {
+    items: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Menu", // Reference to your Menu model
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+        },
+        description: {
+          type: String,
+        },
+        qty: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    customer: {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        match: /.+\@.+\..+/,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: Number,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-// module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
