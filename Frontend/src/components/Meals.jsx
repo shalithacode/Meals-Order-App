cd2ef1e4e6aeaa2b846cbec5bda4a4117ea9b3a4
shalith98 <shalithalakruwan1998@gmail.com>
2025-05-25 23:16:26 +0530

    Added Error component

--- Frontend/src/components/Meals.jsx
+++ Frontend/src/components/Meals.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState, useMemo } from "react";
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
-
+import Error from "./Error";
 const URL = "http://localhost:8080/meals";
 function Meals() {
   const config = useMemo(() => ({ method: "GET" }), []);
   const { data: loadedMeals, isLoading, error } = useHttp(URL, config);
 
   if (isLoading) return <p className="center">Loading Meals...</p>;
+  if (error) return <Error title="Failed to fetch meals." message={error} />;
   return (
     <ul id="meals">
       {loadedMeals &&
